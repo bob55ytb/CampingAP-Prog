860ec282b7acf728c5ecb07c6fee7e3becc46be7
Ney <neatixtiktok@gmail.com>
2026-09-24 14:11:54 +0200

    Update Class

---
+++ src/Animateur.java
@@ -0,0 +1,25 @@
+public class Animateur {
+
+    private String NomAnimateur;
+    private String PrenomAnimateur;
+    private String GmailAnimateur;
+    private String CodePostalAnimateur;
+    private String RueAnimateur;
+    private String VilleAnimateur;
+    private String TelephoneAnimateur;
+
+    public Animateur(String NomAnimateur, String PrenomAnimateur, String GmailAnimateur, String CodePostalAnimateur, String RueAnimateur, String VilleAnimateur, String TelephoneAnimateur){
+
+        this.CodePostalAnimateur = CodePostalAnimateur;
+        this.NomAnimateur = NomAnimateur;
+        this.PrenomAnimateur = PrenomAnimateur;
+        this.GmailAnimateur = GmailAnimateur;
+        this.RueAnimateur = RueAnimateur;
+        this.VilleAnimateur = VilleAnimateur;
+        this.TelephoneAnimateur = TelephoneAnimateur;
+
+
+    }
+
+
+}
