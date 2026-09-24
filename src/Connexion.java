06bf3ca3ab3bbe7133df8e7ea3090ca225a13adb
Yubyr <nathansallerin@gmail.com>
2026-09-24 13:55:20 +0200

    Update 2

---
+++ src/Connexion.java
@@ -0,0 +1,18 @@
+public class Connexion {
+    int id;
+    String user;
+    String password;
+    int perm;
+
+    public Connexion(int id,String user , String password, int perm){
+        this.id         = id;
+        this.user       = user;
+        this.password   = password;
+        this.perm       = perm;
+    }
+
+    public void Connect(String user , String password){
+
+    }
+
+}
---
+++ src/DBase.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBase {
+    private static final String URL = "172.17.0.18";
+    private static final String UTILISATEUR = "Nathan";
+    private static final String MOT_DE_PASSE = "0550002D";
+
+
+}
