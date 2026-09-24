1c5619146ab9a0f2fd0c47bcf0204e67b46c3d18
Ney <neatixtiktok@gmail.com>
2026-09-24 14:42:54 +0200

    Update Class

--- src/Animateur.java
+++ src/Animateur.java
@@ -1,25 +1,78 @@
 public class Animateur {
 
-    private String NomAnimateur;
-    private String PrenomAnimateur;
-    private String GmailAnimateur;
-    private String CodePostalAnimateur;
-    private String RueAnimateur;
-    private String VilleAnimateur;
-    private String TelephoneAnimateur;
+    private String nomAnimateur;
+    private String prenomAnimateur;
+    private String gmailAnimateur;
+    private int codePostalAnimateur;
+    private String rueAnimateur;
+    private String villeAnimateur;
+    private int telephoneAnimateur;
 
-    public Animateur(String NomAnimateur, String PrenomAnimateur, String GmailAnimateur, String CodePostalAnimateur, String RueAnimateur, String VilleAnimateur, String TelephoneAnimateur){
+    public Animateur(String nomAnimateur, String prenomAnimateur, String gmailAnimateur, int codePostalAnimateur, String rueAnimateur, String villeAnimateur, int telephoneAnimateur){
 
-        this.CodePostalAnimateur = CodePostalAnimateur;
-        this.NomAnimateur = NomAnimateur;
-        this.PrenomAnimateur = PrenomAnimateur;
-        this.GmailAnimateur = GmailAnimateur;
-        this.RueAnimateur = RueAnimateur;
-        this.VilleAnimateur = VilleAnimateur;
-        this.TelephoneAnimateur = TelephoneAnimateur;
-
+        this.codePostalAnimateur = codePostalAnimateur;
+        this.nomAnimateur = nomAnimateur;
+        this.prenomAnimateur = prenomAnimateur;
+        this.gmailAnimateur = gmailAnimateur;
+        this.rueAnimateur = rueAnimateur;
+        this.villeAnimateur =villeAnimateur;
+        this.telephoneAnimateur = telephoneAnimateur;
 
     }
 
+    public String getPrenomAnimateur() {
+        return prenomAnimateur;
+    }
 
+    public String getGmailAnimateur() {
+        return gmailAnimateur;
+    }
+
+    public String getCodePostalAnimateur() {
+        return codePostalAnimateur;
+    }
+
+    public String getRueAnimateur() {
+        return rueAnimateur;
+    }
+
+    public String getVilleAnimateur() {
+        return villeAnimateur;
+    }
+
+    public String getTelephoneAnimateur() {
+        return telephoneAnimateur;
+    }
+
+    public String getNomAnimateur() {
+        return nomAnimateur;
+    }
+
+    public void setNomAnimateur(String nomAnimateur) {
+        nomAnimateur = nomAnimateur;
+    }
+
+    public void setPrenomAnimateur(String prenomAnimateur) {
+        prenomAnimateur = prenomAnimateur;
+    }
+
+    public void setGmailAnimateur(String gmailAnimateur) {
+        gmailAnimateur = gmailAnimateur;
+    }
+
+    public void setCodePostalAnimateur(String codePostalAnimateur) {
+        codePostalAnimateur = codePostalAnimateur;
+    }
+
+    public void setRueAnimateur(String rueAnimateur) {
+        rueAnimateur = rueAnimateur;
+    }
+
+    public void setVilleAnimateur(String villeAnimateur) {
+        villeAnimateur = villeAnimateur;
+    }
+
+    public void setTelephoneAnimateur(String telephoneAnimateur) {
+        telephoneAnimateur = telephoneAnimateur;
+    }
 }
--- src/Animation.java
+++ src/Animation.java
@@ -1,2 +1,64 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Animation {
+
+
+    private String nomAnimation;
+    private LocalDate dateAnimation;
+    private LocalTime debutHeureAnimation;
+    private LocalTime finHeureAnimation;
+    // Déclaration de la liste
+    private ArrayList<Animateur> lesAnimateurs;
+
+    public Animation(String nomAnimation, LocalDate dateAnimation, LocalTime debutHeureAnimation, LocalTime finHeureAnimation){
+        this.dateAnimation = dateAnimation;
+        this.nomAnimation = nomAnimation;
+        this.debutHeureAnimation = debutHeureAnimation;
+        this.finHeureAnimation = finHeureAnimation;
+
+        // On crée la liste, elle est actuellement vide
+        this.lesAnimateurs = new ArrayList<>();
+    }
+
+    public String getNomAnimation() {
+        return nomAnimation;
+    }
+
+    public ArrayList<Animateur> getLesAnimateurs() {
+        return lesAnimateurs;
+    }
+
+    public LocalTime getFinHeureAnimation() {
+        return finHeureAnimation;
+    }
+
+    public LocalDate getDateAnimation() {
+        return dateAnimation;
+    }
+
+    public LocalTime getDebutHeureAnimation() {
+        return debutHeureAnimation;
+    }
+
+    public void setNomAnimation(String nomAnimation) {
+        this.nomAnimation = nomAnimation;
+    }
+
+    public void setDateAnimation(LocalDate dateAnimation) {
+        this.dateAnimation = dateAnimation;
+    }
+
+    public void setDebutHeureAnimation(LocalTime debutHeureAnimation) {
+        this.debutHeureAnimation = debutHeureAnimation;
+    }
+
+    public void setFinHeureAnimation(LocalTime finHeureAnimation) {
+        this.finHeureAnimation = finHeureAnimation;
+    }
+
+    public void setLesAnimateurs(ArrayList<Animateur> lesAnimateurs) {
+        this.lesAnimateurs = lesAnimateurs;
+    }
 }
